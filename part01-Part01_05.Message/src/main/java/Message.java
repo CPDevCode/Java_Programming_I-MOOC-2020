63720c387aaa4b877dbb0350459e457536b23f5e
Christiancodeprojects <74272660+Christiancodeprojects@users.noreply.github.com>
2020-11-17 16:31:02 -0700

    Java

---
+++ part01-Part01_05.Message/src/main/java/Message.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Message {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+        // Write your program here
+        //get's message and prints it back to user
+        String useMessage = scanner.nextLine();
+        System.out.println(useMessage);
+
+    }
+}
